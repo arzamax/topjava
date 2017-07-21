363b60d2b1e93e3013bb867fb8e8c3d7e397db34
Aleksandr <shuto4ka1@gmail.com>
2017-07-21 22:53:19 +0300

    Added login form.

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -1,8 +1,13 @@
 package ru.javawebinar.topjava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class AuthorizedUser {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthorizedUser.class);
 
     private static int id = 1;
 
@@ -11,6 +16,7 @@ public class AuthorizedUser {
     }
 
     public static void setId(int id) {
+        log.info("Authorized user sets to {}", id);
         AuthorizedUser.id = id;
     }
 
